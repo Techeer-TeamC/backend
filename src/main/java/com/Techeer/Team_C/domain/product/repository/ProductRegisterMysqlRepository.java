f49725c3429b7f96bd4cc650105b93ae4d43a363
Somi Kim <rlathal009@gmail.com>
2022-06-04 02:10:00 +0900

    feat : create ProductRegisterRepository

--- src/main/java/com/Techeer/Team_C/domain/product/repository/ProductRegisterMysqlRepository.java
+++ src/main/java/com/Techeer/Team_C/domain/product/repository/ProductRegisterMysqlRepository.java
@@ -1,7 +1,26 @@
 package com.Techeer.Team_C.domain.product.repository;
 
+import com.Techeer.Team_C.domain.product.entity.Product;
 import com.Techeer.Team_C.domain.product.entity.ProductRegister;
+import com.Techeer.Team_C.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface ProductRegisterMysqlRepository extends JpaRepository<ProductRegister, Long> {
+
+    List<ProductRegister> findAllByUser(User user);
+
+    @Transactional
+    default ProductRegister build(User user, Product product, int desiredPrice, boolean status) {
+        ProductRegister productRegister = ProductRegister.builder()
+                .user(user)
+                .product(product)
+                .desiredPrice(desiredPrice)
+                .status(status)
+                .build();
+
+        return saveAndFlush(productRegister);
+    }
 }
